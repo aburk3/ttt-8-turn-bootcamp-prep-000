85c32374059a117575c3d5ae890a09c31b46845f
Austin  Burke <aburk3@gmail.com>
2018-09-10 17:57:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,5 +28,13 @@ def position_taken?(board, index)
 end
 
 def turn(board)
-    puts "Please enter 1-9:"
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
